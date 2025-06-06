16da59d8ecc12db0fcaa41373a05fcd63f5a5dc4
𝘡𝘌𝘡𝘌47 𝘛𝘌𝘊𝘏 <humphreymbise70@gmail.com>
2025-06-06 23:58:34 +0300

    Update quranmenu.js

--- zezeplugins/quranmenu.js
+++ zezeplugins/quranmenu.js
@@ -399,8 +399,8 @@ let menuMsg = `
                 externalAdReply: {
                     title: "ZEZE47-MD SURAH LIST",
                     body: "Live with God my friends you don't know your tomorrow",
-                    thumbnailUrl: "https://files.catbox.moe/aktbgo.jpg",
-                    sourceUrl: "https://whatsapp.com/channel/0029VaeRrcnADTOKzivM0S1r",
+                    thumbnailUrl: "https://files.catbox.moe/9q4ueg.jpg",
+                    sourceUrl: "https://whatsapp.com/channel/0029VbANIT5D8SDpK7oExi1v",
                     mediaType: 1,
                     renderLargerThumbnail: true
                 }
